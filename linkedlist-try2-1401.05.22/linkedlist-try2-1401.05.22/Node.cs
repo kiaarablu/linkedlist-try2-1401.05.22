4e8fb2ae2e9dadb49a1a39518bed7f3f05d4e73a
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-13 11:40:58 +0430

    Add files via upload

---
+++ linkedlist-try2-1401.05.22/linkedlist-try2-1401.05.22/Node.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist_try2_1401._05._22
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+    }
+    public class NodeProcess
+    {
+        Node head = new Node();
+        Node current;
+        public NodeProcess()
+        {
+            head.value = 1;
+            head.next = null;
+            current = head;
+        }
+        public void PushFirstNode(int val)
+        {
+            Node Fadd = new Node();
+            Fadd.value = val;
+            head.next = head;
+            head = Fadd;    
+        }
+
+        public void PushLastNode(int val)
+        {
+            Node lastadd = new Node();
+            lastadd.value = val;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = lastadd;
+        }
+        public void PrintNode()
+        {
+            var crnt = head;
+            while (crnt != null)
+            {
+                Console.WriteLine(current.value);
+                current = current.next;
+            }
+
+        }
+
+
+    }
+    
+}
---
+++ linkedlist-try2-1401.05.22/linkedlist-try2-1401.05.22/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist_try2_1401._05._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
